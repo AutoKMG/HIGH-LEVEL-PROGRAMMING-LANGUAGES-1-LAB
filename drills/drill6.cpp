4ad5b4a42487a6aedb6ba3adc87a97863663deee
Khaled Galal <42820137+AutoKMG@users.noreply.github.com>
2022-05-07 12:15:44 +0200

    Add files via upload

---
+++ drills/drill6.cpp
@@ -0,0 +1,214 @@
+
+//
+// This is example code from Chapter 6.7 "Trying the second version" of
+// "Software - Principles and Practice using C++" by Bjarne Stroustrup
+//
+
+/*
+    This file is known as calculator02buggy.cpp
+
+    I have inserted 5 errors that should cause this not to compile
+    I have inserted 3 logic errors that should cause the program to give wrong results
+
+    First try to find an remove the bugs without looking in the book.
+    If that gets tedious, compare the code to that in the book (or posted source code)
+
+    Happy hunting!
+
+*/
+
+#include "std_lib_facilities.h"
+
+//------------------------------------------------------------------------------
+
+class Token{ // There was a "c" missing in the word "class"
+public:
+    char kind;        // what kind of token
+    double value;     // for numbers: a value
+    Token(char ch)    // make a Token from a char
+        :kind(ch), value(0) { }
+    Token(char ch, double val)     // make a Token from a char and a double
+        :kind(ch), value(val) { }
+};
+
+//------------------------------------------------------------------------------
+
+class Token_stream {
+public:
+    Token_stream();   // make a Token_stream that reads from cin
+    Token get();      // get a Token (get() is defined elsewhere)
+    void putback(Token t);    // put a Token back
+private:
+    bool full;        // is there a Token in the buffer?
+    Token buffer;     // here is where we keep a Token put back using putback()
+};
+
+//------------------------------------------------------------------------------
+
+// The constructor just sets full to indicate that the buffer is empty:
+Token_stream::Token_stream()
+    :full(false), buffer(0)    // no Token in buffer
+{
+}
+
+//------------------------------------------------------------------------------
+
+// The putback() member function puts its argument back into the Token_stream's buffer:
+void Token_stream::putback(Token t)
+{
+    if (full) error("putback() into a full buffer");
+    buffer = t;       // copy t to buffer
+    full = true;      // buffer is now full
+}
+
+//------------------------------------------------------------------------------
+
+Token Token_stream::get() // The error that was no reference to Token_stream
+{
+    if (full) {       // do we already have a Token ready?
+        // remove token from buffer
+        full = false;
+        return buffer;
+    }
+
+    char ch;
+    cin >> ch;    // note that >> skips whitespace (space, newline, tab, etc.)
+
+    switch (ch) {
+    case '=':    // for "print" we changed from ";" to "="
+    case 'x':    // for "quit" we changed from "q" to "x"
+    case '(': case ')': case '+': case '-': case '*': case '/':
+        return Token(ch);        // let each character represent itself
+    case '.':
+    case '0': case '1': case '2': case '3': case '4':
+    case '5': case '6': case '7': case '9':
+    {
+        cin.putback(ch);         // put digit back into the input stream
+        double val;
+        cin >> val;              // read a floating-point number
+        return Token('8', val);   // let '8' represent "a number"
+    }
+    default:
+        error("Bad token");
+    }
+}
+
+//------------------------------------------------------------------------------
+
+Token_stream ts;        // provides get() and putback()
+
+//------------------------------------------------------------------------------
+
+double expression();    // declaration so that primary() can call expression()
+
+//------------------------------------------------------------------------------
+
+// deal with numbers and parentheses
+double primary()
+{
+    Token t = ts.get();
+    switch (t.kind) {
+    case '(':    // handle '(' expression ')'
+    {
+        double d = expression();
+        t = ts.get();
+        if (t.kind != ')') error("')' expected"); // There was an " missing after expected
+            return d;
+    }
+    case '8':            // we use '8' to represent a number
+        return t.value;  // return the number's value
+    default:
+        error("primary expected");
+    }
+}
+
+//------------------------------------------------------------------------------
+
+// deal with *, /, and %
+double term()
+{
+    double left = primary();
+    Token t = ts.get();        // get the next token from token stream
+
+    while (true) {
+        switch (t.kind) {
+        case '*':
+            left *= primary();
+            t = ts.get();
+            break; // There was a break missing here
+        case '/':
+        {
+            double d = primary();
+            if (d == 0) error("divide by zero");
+            left /= d;
+            t = ts.get();
+            break;
+        }
+        default:
+            ts.putback(t);     // put t back into the token stream
+            return left;
+        }
+    }
+}
+
+//------------------------------------------------------------------------------
+
+// deal with + and -
+double expression()
+{
+    double left = term(); // Error: Missing )      // read and evaluate a Term
+    Token t = ts.get();        // get the next token from token stream
+
+    while (true) {
+        switch (t.kind) {
+        case '+':
+            left += term();    // evaluate Term and add
+            t = ts.get();
+            break;
+        case '-':
+            left -= term();  // Logical Error: from += to -=  // evaluate Term and subtract
+            t = ts.get();
+            break;
+        default:
+            ts.putback(t);     // put t back into the token stream
+            return left;       // finally: no more + or -: return the answer
+        }
+    }
+}
+
+//------------------------------------------------------------------------------
+
+int main()
+{
+    cout << "Welcome to our simple calculator.\n";
+    cout << "Please enter expressions using floating-point numbers.\n";
+    cout << "In this application you can add, subtract, multiply, or divide.\n";
+    cout << "You can use the symbol = to get your result.\n";
+    cout << "You can use x to quit the application.\n";
+    double val = 0.0;
+    try
+    {
+        while (cin) {
+            Token t = ts.get();
+
+            if (t.kind == 'x') break; // 'q' for quit
+            if (t.kind == '=')        // ';' for "print now"
+                cout << "=" << val << '\n';
+            else
+                ts.putback(t);
+            val = expression();
+        }
+        keep_window_open();
+    }
+    catch (exception& e) {
+        cerr << "error: " << e.what() << '\n';
+        keep_window_open();
+        return 1;
+    }
+    catch (...) {
+        cerr << "Oops: unknown exception!\n";
+        keep_window_open();
+        return 2;
+    }
+}
+//------------------------------------------------------------------------------
